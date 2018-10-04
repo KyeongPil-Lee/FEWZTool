626b84ae0a1f053c27b56ba8c264940907857358
KyeongPil-Lee <kplee@cern.ch>
2018-10-04 20:40:16 +0900

    add node info

--- Workspace/v01_TestForBinOpti_NLO/qsub_TestForBinOpti_NLO.sh
+++ Workspace/v01_TestForBinOpti_NLO/qsub_TestForBinOpti_NLO.sh
@@ -2,43 +2,45 @@
 # -- (after customization) just copy the commands and type in the terminal
 
 cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v01_TestForBinOpti_NLO
-qsub TestForBinOpti_NLO_v00.sh
-qsub TestForBinOpti_NLO_v01.sh
-qsub TestForBinOpti_NLO_v02.sh
-qsub TestForBinOpti_NLO_v03.sh
-qsub TestForBinOpti_NLO_v04.sh
-qsub TestForBinOpti_NLO_v05.sh
-qsub TestForBinOpti_NLO_v06.sh
-qsub TestForBinOpti_NLO_v07.sh
-qsub TestForBinOpti_NLO_v08.sh
-qsub TestForBinOpti_NLO_v09.sh
-qsub TestForBinOpti_NLO_v10.sh
-qsub TestForBinOpti_NLO_v11.sh
-qsub TestForBinOpti_NLO_v12.sh
-qsub TestForBinOpti_NLO_v13.sh
-qsub TestForBinOpti_NLO_v14.sh
-qsub TestForBinOpti_NLO_v15.sh
-qsub TestForBinOpti_NLO_v16.sh
-qsub TestForBinOpti_NLO_v17.sh
-qsub TestForBinOpti_NLO_v18.sh
-qsub TestForBinOpti_NLO_v19.sh
-qsub TestForBinOpti_NLO_v20.sh
-qsub TestForBinOpti_NLO_v21.sh
-qsub TestForBinOpti_NLO_v22.sh
-qsub TestForBinOpti_NLO_v23.sh
-qsub TestForBinOpti_NLO_v24.sh
-qsub TestForBinOpti_NLO_v25.sh
-qsub TestForBinOpti_NLO_v26.sh
-qsub TestForBinOpti_NLO_v27.sh
-qsub TestForBinOpti_NLO_v28.sh
-qsub TestForBinOpti_NLO_v29.sh
-qsub TestForBinOpti_NLO_v30.sh
-qsub TestForBinOpti_NLO_v31.sh
-qsub TestForBinOpti_NLO_v32.sh
-qsub TestForBinOpti_NLO_v33.sh
-qsub TestForBinOpti_NLO_v34.sh
-qsub TestForBinOpti_NLO_v35.sh
-qsub TestForBinOpti_NLO_v36.sh
-qsub TestForBinOpti_NLO_v37.sh
-qsub TestForBinOpti_NLO_v38.sh
-qsub TestForBinOpti_NLO_v39.sh
+
+# qsub -l vnode=node1 TestForBinOpti_NLO_v00.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v01.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v02.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v03.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v04.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v05.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v06.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v07.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v08.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v09.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v10.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v11.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v12.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v13.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v14.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v15.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v16.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v17.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v18.sh
+qsub -l vnode=node1 TestForBinOpti_NLO_v19.sh
+
+qsub -l vnode=node5 TestForBinOpti_NLO_v20.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v21.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v22.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v23.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v24.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v25.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v26.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v27.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v28.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v29.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v30.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v31.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v32.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v33.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v34.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v35.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v36.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v37.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v38.sh
+qsub -l vnode=node5 TestForBinOpti_NLO_v39.sh
