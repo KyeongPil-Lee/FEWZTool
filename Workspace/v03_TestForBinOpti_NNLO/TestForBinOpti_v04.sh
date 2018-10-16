94f1df81160e2414679702470dc4ae125cedb758
Kyoengpil Lee <kplee@cern.ch>
2018-10-16 22:10:16 +0200

    use bash (not source) and submit again

--- Workspace/v03_TestForBinOpti_NNLO/TestForBinOpti_v04.sh
+++ Workspace/v03_TestForBinOpti_NNLO/TestForBinOpti_v04.sh
@@ -2,13 +2,13 @@
 
 cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v03_TestForBinOpti_NNLO
 
-source v20181007_TestForBinOpti_M60to120_diRap0p9to1p2.sh >&v20181007_TestForBinOpti_M60to120_diRap0p9to1p2.log
-echo "v20181007_TestForBinOpti_M60to120_diRap0p9to1p2.sh: finished"
+# bash v20181007_TestForBinOpti_M60to120_diRap0p9to1p2.sh >&v20181007_TestForBinOpti_M60to120_diRap0p9to1p2.log
+# echo "v20181007_TestForBinOpti_M60to120_diRap0p9to1p2.sh: finished"
 
-source v20181007_TestForBinOpti_M60to120_diRap1p2to1p5.sh >&v20181007_TestForBinOpti_M60to120_diRap1p2to1p5.log
+bash v20181007_TestForBinOpti_M60to120_diRap1p2to1p5.sh >&v20181007_TestForBinOpti_M60to120_diRap1p2to1p5.log
 echo "v20181007_TestForBinOpti_M60to120_diRap1p2to1p5.sh: finished"
 
-source v20181007_TestForBinOpti_M60to120_diRap1p5to1p8.sh >&v20181007_TestForBinOpti_M60to120_diRap1p5to1p8.log
+bash v20181007_TestForBinOpti_M60to120_diRap1p5to1p8.sh >&v20181007_TestForBinOpti_M60to120_diRap1p5to1p8.log
 echo "v20181007_TestForBinOpti_M60to120_diRap1p5to1p8.sh: finished"
 
 
--- Workspace/v03_TestForBinOpti_NNLO/TestForBinOpti_v06.sh
+++ Workspace/v03_TestForBinOpti_NNLO/TestForBinOpti_v06.sh
@@ -2,13 +2,13 @@
 
 cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v03_TestForBinOpti_NNLO
 
-source v20181007_TestForBinOpti_M120to1000_diRap0p0to0p3.sh >&v20181007_TestForBinOpti_M120to1000_diRap0p0to0p3.log
-echo "v20181007_TestForBinOpti_M120to1000_diRap0p0to0p3.sh: finished"
+# bash v20181007_TestForBinOpti_M120to1000_diRap0p0to0p3.sh >&v20181007_TestForBinOpti_M120to1000_diRap0p0to0p3.log
+# echo "v20181007_TestForBinOpti_M120to1000_diRap0p0to0p3.sh: finished"
 
-source v20181007_TestForBinOpti_M120to1000_diRap0p3to0p6.sh >&v20181007_TestForBinOpti_M120to1000_diRap0p3to0p6.log
+bash v20181007_TestForBinOpti_M120to1000_diRap0p3to0p6.sh >&v20181007_TestForBinOpti_M120to1000_diRap0p3to0p6.log
 echo "v20181007_TestForBinOpti_M120to1000_diRap0p3to0p6.sh: finished"
 
-source v20181007_TestForBinOpti_M120to1000_diRap0p6to0p9.sh >&v20181007_TestForBinOpti_M120to1000_diRap0p6to0p9.log
+bash v20181007_TestForBinOpti_M120to1000_diRap0p6to0p9.sh >&v20181007_TestForBinOpti_M120to1000_diRap0p6to0p9.log
 echo "v20181007_TestForBinOpti_M120to1000_diRap0p6to0p9.sh: finished"
 
 
