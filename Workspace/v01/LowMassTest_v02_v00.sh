83a2da7c71f3a346f3a78da1611ea296126c2469
Kyoengpil Lee <kplee@cern.ch>
2018-10-17 22:51:46 +0900

    inputs for 14.5GeV case

---
+++ Workspace/v01/LowMassTest_v02_v00.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/kplee/Physics/FEWZTool/v02_LowMass/Workspace/v01
+
+bash v20181017_LowMassTest_v02_M14to50.sh >&v20181017_LowMassTest_v02_M14to50.log
+echo "v20181017_LowMassTest_v02_M14to50.sh: finished"
+
+
+echo "ALL: finished"
---
+++ Workspace/v01/qsub_LowMassTest_v02.sh
@@ -0,0 +1,5 @@
+# -- NOT intended for sourcing ...
+# -- (after customization) just copy the commands and type in the terminal
+
+cd /home/kplee/Physics/FEWZTool/v02_LowMass/Workspace/v01
+qsub LowMassTest_v02_v00.sh
