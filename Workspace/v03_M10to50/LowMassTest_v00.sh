18e516ee52ac03e9fc2b99233d1156e809021268
KyeongPil-Lee <kplee@cern.ch>
2018-10-26 21:56:58 +0900

    input & result

---
+++ Workspace/v03_M10to50/LowMassTest_v00.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data9/Users/kplee/FEWZTool/v02_LowMass/Workspace/v03_M10to50
+
+bash v20181023_LowMassTest_M10to50.sh >&v20181023_LowMassTest_M10to50.log
+echo "v20181023_LowMassTest_M10to50.sh: finished"
+
+
+echo "ALL: finished"
---
+++ Workspace/v03_M10to50/qsub_LowMassTest.sh
@@ -0,0 +1,5 @@
+# -- NOT intended for sourcing ...
+# -- (after customization) just copy the commands and type in the terminal
+
+cd /data9/Users/kplee/FEWZTool/v02_LowMass/Workspace/v03_M10to50
+qsub LowMassTest_v00.sh
