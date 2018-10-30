74872bb2476c53f34e5e05693da58c455ae09ecd
KyeongPil-Lee <kplee@cern.ch>
2018-10-30 20:34:49 +0900

    input & result

---
+++ Workspace/v06_BelowZpeak/v20181030_DYReweight_BelowZPeak_NLO_v08.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v06_BelowZpeak
+
+bash v20181030_DYReweight_BelowZPeak_M30to45_diRap2p4to100p0_NLO.sh >&v20181030_DYReweight_BelowZPeak_M30to45_diRap2p4to100p0_NLO.log
+echo "v20181030_DYReweight_BelowZPeak_M30to45_diRap2p4to100p0_NLO.sh: finished"
+
+
+echo "ALL: finished"
