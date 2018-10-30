de2e9eab2fd60632cdcf13897b412f25fed1c38e
Kyoengpil Lee <kplee@cern.ch>
2018-10-30 12:56:51 +0100

    setup & input for NLO test (Z-peak)

---
+++ Workspace/v07_Zpeak/v20181030_DYReweight_ZPeak_NLO_v02.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v07_Zpeak
+
+bash v20181030_DYReweight_ZPeak_M60to72_diRap1p8to2p4_NLO.sh >&v20181030_DYReweight_ZPeak_M60to72_diRap1p8to2p4_NLO.log
+echo "v20181030_DYReweight_ZPeak_M60to72_diRap1p8to2p4_NLO.sh: finished"
+
+
+echo "ALL: finished"
