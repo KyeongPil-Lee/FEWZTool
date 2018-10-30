21dcd342b789669f435491fb35e97558e812bdee
Kyoengpil Lee <kplee@cern.ch>
2018-10-30 13:29:50 +0100

    setup & input for NLO test (above Z-peak)

---
+++ Workspace/v08_AboveZpeak/v20181030_DYReweight_AboveZPeak_NLO_v18.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v08_AboveZpeak
+
+bash v20181030_DYReweight_AboveZPeak_M200to400_diRap1p5to2p4_NLO.sh >&v20181030_DYReweight_AboveZPeak_M200to400_diRap1p5to2p4_NLO.log
+echo "v20181030_DYReweight_AboveZPeak_M200to400_diRap1p5to2p4_NLO.sh: finished"
+
+
+echo "ALL: finished"
---
+++ Workspace/v08_AboveZpeak/v20181030_DYReweight_AboveZPeak_NLO_v24.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v08_AboveZpeak
+
+bash v20181030_DYReweight_AboveZPeak_M510to600_diRap0p0to0p9_NLO.sh >&v20181030_DYReweight_AboveZPeak_M510to600_diRap0p0to0p9_NLO.log
+echo "v20181030_DYReweight_AboveZPeak_M510to600_diRap0p0to0p9_NLO.sh: finished"
+
+
+echo "ALL: finished"
