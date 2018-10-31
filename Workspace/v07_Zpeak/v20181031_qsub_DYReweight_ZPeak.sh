b4161a7010d05cf55b5da03bef16575ca3cfc35e
Kyoengpil Lee <kplee@cern.ch>
2018-10-31 11:40:48 +0100

    add ncpus options

--- Workspace/v07_Zpeak/v20181031_qsub_DYReweight_ZPeak.sh
+++ Workspace/v07_Zpeak/v20181031_qsub_DYReweight_ZPeak.sh
@@ -2,15 +2,15 @@
 # -- (after customization) just copy the commands and type in the terminal
 
 cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v07_Zpeak
-qsub v20181031_DYReweight_ZPeak_v00.sh
-qsub v20181031_DYReweight_ZPeak_v01.sh
-qsub v20181031_DYReweight_ZPeak_v02.sh
-qsub v20181031_DYReweight_ZPeak_v03.sh
-qsub v20181031_DYReweight_ZPeak_v04.sh
-qsub v20181031_DYReweight_ZPeak_v05.sh
-qsub v20181031_DYReweight_ZPeak_v06.sh
-qsub v20181031_DYReweight_ZPeak_v07.sh
-qsub v20181031_DYReweight_ZPeak_v08.sh
-qsub v20181031_DYReweight_ZPeak_v09.sh
-qsub v20181031_DYReweight_ZPeak_v10.sh
-qsub v20181031_DYReweight_ZPeak_v11.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v00.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v01.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v02.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v03.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v04.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v05.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v06.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v07.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v08.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v09.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v10.sh
+qsub -l ncpus=8 v20181031_DYReweight_ZPeak_v11.sh
--- Workspace/v09_HighMass/v20181031_qsub_DYReweight_HighMass.sh
+++ Workspace/v09_HighMass/v20181031_qsub_DYReweight_HighMass.sh
@@ -2,6 +2,6 @@
 # -- (after customization) just copy the commands and type in the terminal
 
 cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v09_HighMass
-qsub v20181031_DYReweight_HighMass_v00.sh
-qsub v20181031_DYReweight_HighMass_v01.sh
-qsub v20181031_DYReweight_HighMass_v02.sh
+qsub -l ncpus=8 v20181031_DYReweight_HighMass_v00.sh
+qsub -l ncpus=8 v20181031_DYReweight_HighMass_v01.sh
+qsub -l ncpus=8 v20181031_DYReweight_HighMass_v02.sh
