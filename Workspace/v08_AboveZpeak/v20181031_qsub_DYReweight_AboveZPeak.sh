c135d868347a99e11b4a65bbe6486b073bae1ee6
KyeongPil-Lee <kplee@cern.ch>
2018-11-05 18:36:29 +0900

    ncpu option

--- Workspace/v08_AboveZpeak/v20181031_qsub_DYReweight_AboveZPeak.sh
+++ Workspace/v08_AboveZpeak/v20181031_qsub_DYReweight_AboveZPeak.sh
@@ -2,35 +2,35 @@
 # -- (after customization) just copy the commands and type in the terminal
 
 cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v08_AboveZpeak
-qsub v20181031_DYReweight_AboveZPeak_v00.sh
-qsub v20181031_DYReweight_AboveZPeak_v01.sh
-qsub v20181031_DYReweight_AboveZPeak_v02.sh
-qsub v20181031_DYReweight_AboveZPeak_v03.sh
-qsub v20181031_DYReweight_AboveZPeak_v04.sh
-qsub v20181031_DYReweight_AboveZPeak_v05.sh
-qsub v20181031_DYReweight_AboveZPeak_v06.sh
-qsub v20181031_DYReweight_AboveZPeak_v07.sh
-qsub v20181031_DYReweight_AboveZPeak_v08.sh
-qsub v20181031_DYReweight_AboveZPeak_v09.sh
-qsub v20181031_DYReweight_AboveZPeak_v10.sh
-qsub v20181031_DYReweight_AboveZPeak_v11.sh
-qsub v20181031_DYReweight_AboveZPeak_v12.sh
-qsub v20181031_DYReweight_AboveZPeak_v13.sh
-qsub v20181031_DYReweight_AboveZPeak_v14.sh
-qsub v20181031_DYReweight_AboveZPeak_v15.sh
-qsub v20181031_DYReweight_AboveZPeak_v16.sh
-qsub v20181031_DYReweight_AboveZPeak_v17.sh
-qsub v20181031_DYReweight_AboveZPeak_v18.sh
-qsub v20181031_DYReweight_AboveZPeak_v19.sh
-qsub v20181031_DYReweight_AboveZPeak_v20.sh
-qsub v20181031_DYReweight_AboveZPeak_v21.sh
-qsub v20181031_DYReweight_AboveZPeak_v22.sh
-qsub v20181031_DYReweight_AboveZPeak_v23.sh
-qsub v20181031_DYReweight_AboveZPeak_v24.sh
-qsub v20181031_DYReweight_AboveZPeak_v25.sh
-qsub v20181031_DYReweight_AboveZPeak_v26.sh
-qsub v20181031_DYReweight_AboveZPeak_v27.sh
-qsub v20181031_DYReweight_AboveZPeak_v28.sh
-qsub v20181031_DYReweight_AboveZPeak_v29.sh
-qsub v20181031_DYReweight_AboveZPeak_v30.sh
-qsub v20181031_DYReweight_AboveZPeak_v31.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v00.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v01.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v02.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v03.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v04.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v05.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v06.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v07.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v08.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v09.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v10.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v11.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v12.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v13.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v14.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v15.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v16.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v17.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v18.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v19.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v20.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v21.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v22.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v23.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v24.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v25.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v26.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v27.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v28.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v29.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v30.sh
+qsub ncpus=8 v20181031_DYReweight_AboveZPeak_v31.sh
