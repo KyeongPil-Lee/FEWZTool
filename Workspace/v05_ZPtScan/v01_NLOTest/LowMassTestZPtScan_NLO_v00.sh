bb7fd7cada2c5b9d07815f2558fb6a24731a8aa1
KyeongPil-Lee <kplee@cern.ch>
2018-11-20 23:16:32 +0900

    NLO test result

---
+++ Workspace/v05_ZPtScan/v01_NLOTest/LowMassTestZPtScan_NLO_v00.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data9/Users/kplee/FEWZTool/v02_LowMass/Workspace/v05_ZPtScan
+
+bash v20181120_LowMassTestZPtScan_M10p0to50p0_minZPt0p2_NLO.sh >&v20181120_LowMassTestZPtScan_M10p0to50p0_minZPt0p2_NLO.log
+echo "v20181120_LowMassTestZPtScan_M10p0to50p0_minZPt0p2_NLO.sh: finished"
+
+
+echo "ALL: finished"
---
+++ Workspace/v05_ZPtScan/v01_NLOTest/qsub_LowMassTestZPtScan_NLO.sh
@@ -0,0 +1,9 @@
+# -- NOT intended for sourcing ...
+# -- (after customization) just copy the commands and type in the terminal
+
+cd /data9/Users/kplee/FEWZTool/v02_LowMass/Workspace/v05_ZPtScan
+qsub LowMassTestZPtScan_NLO_v00.sh
+qsub LowMassTestZPtScan_NLO_v01.sh
+qsub LowMassTestZPtScan_NLO_v02.sh
+qsub LowMassTestZPtScan_NLO_v03.sh
+qsub LowMassTestZPtScan_NLO_v04.sh
