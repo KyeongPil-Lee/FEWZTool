b103791c8e19e106b78526fb3cfc9be35ab68c68
Kyoengpil Lee <kplee@cern.ch>
2018-12-01 23:06:12 +0100

    test with large initial call number

---
+++ Workspace/v12_BelowZPeak_MergeBelow20GeV/v02_largerNCall/v20181201_DYReweight_BelowZPeak_largerNCall_v00.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v12_BelowZPeak_MergeBelow20GeV/v02_largerNCall
+
+bash v20181201_DYReweight_BelowZPeak_largerNCall_M15to20_diRap2p4to100p0.sh >&v20181201_DYReweight_BelowZPeak_largerNCall_M15to20_diRap2p4to100p0.log
+echo "v20181201_DYReweight_BelowZPeak_largerNCall_M15to20_diRap2p4to100p0.sh: finished"
+
+
+echo "ALL: finished"
---
+++ Workspace/v12_BelowZPeak_MergeBelow20GeV/v02_largerNCall/v20181201_qsub_DYReweight_BelowZPeak_largerNCall.sh
@@ -0,0 +1,5 @@
+# -- NOT intended for sourcing ...
+# -- (after customization) just copy the commands and type in the terminal
+
+cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v12_BelowZPeak_MergeBelow20GeV/v02_largerNCall
+qsub v20181201_DYReweight_BelowZPeak_largerNCall_v00.sh
