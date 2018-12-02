83850b43ffbeae1adc3c230fc54224c12d9b5075
KyeongPil-Lee <kplee@cern.ch>
2018-12-03 05:19:33 +0900

    results

--- Workspace/v12_BelowZPeak_MergeBelow20GeV/v03_absUnc/v20181201_qsub_DYReweight_BelowZPeak_absUnc.sh
+++ Workspace/v12_BelowZPeak_MergeBelow20GeV/v03_absUnc/v20181201_qsub_DYReweight_BelowZPeak_absUnc.sh
@@ -2,4 +2,4 @@
 # -- (after customization) just copy the commands and type in the terminal
 
 cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v12_BelowZPeak_MergeBelow20GeV/v03_absUnc
-qsub v20181201_DYReweight_BelowZPeak_absUnc_v00.sh
+qsub -l ncpus=48 v20181201_DYReweight_BelowZPeak_absUnc_v00.sh
