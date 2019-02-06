d4bc83bd4dcf8bfcda9ff3f92367f2d3ee635786
kplee <kplee@cern.ch>
2019-02-06 12:37:20 +0100

    FEWZ inputs

---
+++ Workspace/v01_electron/InputGenerator.py
@@ -0,0 +1,70 @@
+from Python.FEWZInputGenerator import FEWZInputGenerator
+
+doNLOTest = True
+
+list_PDF = [
+"NNPDF31_nnlo_as_0118_luxqed",
+"CT14nnlo_as_0118",
+]
+
+for PDF in list_PDF:
+    generator = FEWZInputGenerator()
+
+    # -- mandatory options
+    generator.tag = "FiducialXSec_Zpeak_%s" % (PDF)
+
+    # -- 8, 12, 24 ...
+    generator.nCore = 24
+
+    generator.FEWZPath = "/home/kplee/Physics/FEWZ/LHAPDF_621/FEWZ_3.1.rc" # -- absolute path!
+    generator.WSPath = "/data9/Users/kplee/FEWZTool/v05_rapidityInFid/Workspace/v01_electron" # -- absolute path!
+
+    generator.leptonMass = 0.00051099895 # -- electron
+
+    generator.muR = 91.19
+    generator.muF = 91.19
+
+    generator.relUnc = 0.1 # -- or generator.absUnc = XXX (absolute uncertainty is preferred)
+
+    generator.QCDOrder = 2 # -- QCD@NNLO
+    generator.EWKOrder = 1 # -- EWK@NLO
+
+    generator.minM = 60
+    generator.maxM = 120
+
+    generator.minPtSub = 17.0
+    generator.maxPtSub = 100000.0
+    generator.minPtLead = 28.0
+    generator.maxPtLead = 100000.0
+    generator.minEtaSub = 0.0
+    generator.maxEtaSub = 2.4
+    generator.minEtaLead = 0.0
+    generator.maxEtaLead = 2.4
+
+    generator.ZPoleFocus = 1
+
+    generator.scaleVarBy2 = 0 # -- 2 more calculations if it is turned on
+    generator.EWControl = 0 # -- Turn on all ISR/FSR effect
+    generator.dRDressed = 0.0 # -- no dressing
+
+    generator.PDF = PDF
+
+    generator.useCustomHist = True
+    generator.customHistPath = "./HistTemplate/histTemplate.txt"
+
+    # -- NLO as a test
+    if doNLOTest:
+        # -- use NLO PDFs
+        if "NNPDF" in generator.PDF:
+            generator.PDF = "NNPDF31_nlo_as_0118"
+        if "CT14" in generator.PDF:
+            generator.PDF = "CT14nlo_as_0118"
+
+        generator.tag = "FiducialXSec_Zpeak_%s" % (generator.PDF)
+        generator.tag = generator.tag + "_NLO"
+        generator.nCore = 1
+        generator.QCDOrder = 1 # -- QCD@NLO
+        generator.EWKOrder = 0 # -- EWK@LO
+
+    generator.Generate()
+
---
+++ Workspace/v01_electron/v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+start=`date +%s`
+
+export SCRAM_ARCH=slc6_amd64_gcc630
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+# -- CMSSW enviornment -- #
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_10_1_9 # -- has NNPDF3.1+luxQED PDF set
+eval `scramv1 runtime -sh` # -- cmsenv
+
+cd /home/kplee/Physics/FEWZ/LHAPDF_621/FEWZ_3.1.rc/bin
+
+# -- copy all necessary inputs under FEWZ/bin path
+cp /data9/Users/kplee/FEWZTool/v05_rapidityInFid/Workspace/v01_electron/v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO_param.txt ./
+cp /data9/Users/kplee/FEWZTool/v05_rapidityInFid/Workspace/v01_electron/v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO_hist.txt ./
+
+echo "run local_run.sh"
+./local_run.sh z \
+v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO \
+v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO_param.txt \
+v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO_hist.txt \
+v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO.dat \
+../ \
+1
+
+echo "run finish.sh"
+./finish.sh \
+v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO \
+NLO.v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO.dat
+
+# -- bring the output .dat file to Workspace
+cp NLO.v20190206_FiducialXSec_Zpeak_CT14nlo_as_0118_NLO.dat /data9/Users/kplee/FEWZTool/v05_rapidityInFid/Workspace/v01_electron
+
+echo "job is completed"
+
+end=`date +%s`
+
+runtime=$((end-start))
+
+echo "   start:   "$start
+echo "   end:     "$end
+echo "   runtime: "$runtime
+
