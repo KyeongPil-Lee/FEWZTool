35e80307e7464cabedfa5819fc849278dc519c91
kplee <kplee@cern.ch>
2019-02-11 19:50:24 +0900

    update PDF sets

---
+++ Workspace/v01_electron/v20190211_FiducialXSec_Zpeak_CT14nnlo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+start=`date +%s`
+
+export SCRAM_ARCH=slc6_amd64_gcc630
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+# -- CMSSW enviornment -- #
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_10_1_9 # -- has NNPDF3.1+luxQED PDF set
+eval `scramv1 runtime -sh` # -- cmsenv
+
+cd /home/kplee/Physics/FEWZ/LHAPDF_621/FEWZ_3.1.rc/bin
+
+# -- copy all necessary inputs under FEWZ/bin path
+cp /data9/Users/kplee/FEWZTool/v05_rapidityInFid/Workspace/v01_electron/v20190211_FiducialXSec_Zpeak_CT14nnlo_param.txt ./
+cp /data9/Users/kplee/FEWZTool/v05_rapidityInFid/Workspace/v01_electron/v20190211_FiducialXSec_Zpeak_CT14nnlo_hist.txt ./
+
+echo "run local_run.sh"
+./local_run.sh z \
+v20190211_FiducialXSec_Zpeak_CT14nnlo \
+v20190211_FiducialXSec_Zpeak_CT14nnlo_param.txt \
+v20190211_FiducialXSec_Zpeak_CT14nnlo_hist.txt \
+v20190211_FiducialXSec_Zpeak_CT14nnlo.dat \
+../ \
+24
+
+echo "run finish.sh"
+./finish.sh \
+v20190211_FiducialXSec_Zpeak_CT14nnlo \
+NNLO.v20190211_FiducialXSec_Zpeak_CT14nnlo.dat
+
+# -- bring the output .dat file to Workspace
+cp NNLO.v20190211_FiducialXSec_Zpeak_CT14nnlo.dat /data9/Users/kplee/FEWZTool/v05_rapidityInFid/Workspace/v01_electron
+
+echo "job is completed"
+
+end=`date +%s`
+
+runtime=$((end-start))
+
+echo "   start:   "$start
+echo "   end:     "$end
+echo "   runtime: "$runtime
+
