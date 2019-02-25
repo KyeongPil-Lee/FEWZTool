62b7a311967f1fb19a3f5a0a0e35ea18aa635803
kplee <kplee@cern.ch>
2019-02-25 12:59:09 +0900

    add ncpus

--- Workspace/v22_postFSR_HighMass/v20190225_qsub_DYReweight_HighMass.sh
+++ Workspace/v22_postFSR_HighMass/v20190225_qsub_DYReweight_HighMass.sh
@@ -2,6 +2,6 @@
 # -- (after customization) just copy the commands and type in the terminal
 
 cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v22_postFSR_HighMass
-qsub v20190225_DYReweight_HighMass_v00.sh
-qsub v20190225_DYReweight_HighMass_v01.sh
-qsub v20190225_DYReweight_HighMass_v02.sh
+qsub -l ncpus=8 v20190225_DYReweight_HighMass_v00.sh
+qsub -l ncpus=8 v20190225_DYReweight_HighMass_v01.sh
+qsub -l ncpus=8 v20190225_DYReweight_HighMass_v02.sh
