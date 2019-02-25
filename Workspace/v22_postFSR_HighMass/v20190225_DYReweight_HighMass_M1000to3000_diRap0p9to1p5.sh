a720f6464cf29fa8b654f1a4753e2c245daf5db9
kplee <kplee@cern.ch>
2019-02-25 12:58:10 +0900

    inputs

---
+++ Workspace/v22_postFSR_HighMass/v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+start=`date +%s`
+
+export SCRAM_ARCH=slc6_amd64_gcc630
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+# -- CMSSW enviornment -- #
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_10_1_9 # -- has NNPDF3.1+luxQED PDF set
+eval `scramv1 runtime -sh` # -- cmsenv
+
+cd /home/kplee/Physics/FEWZ/v03_absRapCut/FEWZ_3.1.rc/bin
+
+# -- copy all necessary inputs under FEWZ/bin path
+cp /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v22_postFSR_HighMass/v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5_param.txt ./
+cp /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v22_postFSR_HighMass/v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5_hist.txt ./
+cp /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v22_postFSR_HighMass/binDiPt_HighMass_v3.txt ./
+cp /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v22_postFSR_HighMass/binDiRap_HighMass_v3.txt ./
+
+echo "run local_run.sh"
+./local_run.sh z \
+v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5 \
+v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5_param.txt \
+v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5_hist.txt \
+v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5.dat \
+../ \
+8
+
+echo "run finish.sh"
+./finish.sh \
+v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5 \
+NNLO.v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5.dat
+
+# -- bring the output .dat file to Workspace
+cp NNLO.v20190225_DYReweight_HighMass_M1000to3000_diRap0p9to1p5.dat /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v22_postFSR_HighMass
+
+echo "job is completed"
+
+end=`date +%s`
+
+runtime=$((end-start))
+
+echo "   start:   "$start
+echo "   end:     "$end
+echo "   runtime: "$runtime
+
---
+++ Workspace/v22_postFSR_HighMass/v20190225_DYReweight_HighMass_v00.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data9/Users/kplee/FEWZTool/v01_Reweighting/Workspace/v22_postFSR_HighMass
+
+bash v20190225_DYReweight_HighMass_M1000to3000_diRap0p0to0p9.sh >&v20190225_DYReweight_HighMass_M1000to3000_diRap0p0to0p9.log
+echo "v20190225_DYReweight_HighMass_M1000to3000_diRap0p0to0p9.sh: finished"
+
+
+echo "ALL: finished"
